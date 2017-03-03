3cb40c7b7d965d3bff05dbf773bbe378f5511ad0
Kirill Izludec <izludec@hotmail.com>
2017-03-03 16:42:17 +0000

    Perfomance improvements in get_closest_bar

--- bars.py
+++ bars.py
@@ -2,6 +2,8 @@ import json
 import math
 from urllib.request import urlopen
 
+km_in_longtitude = 63
+km_in_latitude = 111
 
 def load_data(filepath):
     bars_from_url = urlopen(filepath)
@@ -11,29 +13,23 @@ def load_data(filepath):
 
 def get_biggest_bar(bar_list):
     return max(bar_list, 
-    key = lambda bar: bar["Cells"]["SeatsCount"])["Cells"]["Name"]
+    key = lambda bar: bar["Cells"]["SeatsCount"])
 
 
 def get_smallest_bar(bar_list):
     return min(bar_list, 
-    key = lambda bar: bar["Cells"]["SeatsCount"])["Cells"]["Name"]
+    key = lambda bar: bar["Cells"]["SeatsCount"])
 
+
+def get_distance_to_bar(bar, longitude, latitude):
+    x_diff = (longitude-bar["Cells"]["geoData"]["coordinates"][0])*km_in_longtitude
+    y_diff = (latitude-bar["Cells"]["geoData"]["coordinates"][1])*km_in_latitude
+    return (math.sqrt((x_diff**2)+(y_diff**2)))
 
 def get_closest_bar(bar_list, longitude, latitude):
-    min_distance = float(50000)
-    distance = 0
-    min_distance_bar = 0
-    list_closest_bar = []
-    for i, bars in enumerate(bar_list):
-        x_diff = (longitude-bars["Cells"]["geoData"]["coordinates"][0])*63
-        y_diff = (latitude-bars["Cells"]["geoData"]["coordinates"][1])*111
-        distance = (math.sqrt((x_diff**2)+(y_diff**2)))
-        if distance < min_distance and distance != 0:
-            min_distance = distance
-            min_distance_bar = i
-    list_closest_bar.append(bar_list[min_distance_bar]["Cells"]["Name"])
-    list_closest_bar.append(min_distance)
-    return list_closest_bar
+    return min(bar_list, 
+    key = lambda bar: get_distance_to_bar(bar, longitude, latitude))
+
 
 
 def get_version():
@@ -44,46 +40,45 @@ def get_version():
 
 
 def get_id():
-    url_bars_id = "http://api.data.mos.ru/v{version}/datasets".format(
+    url_datasets = "http://api.data.mos.ru/v{version}/datasets".format(
         version = str(get_version()))
-    json_bars_id = urlopen(url_bars_id)
-    bars_id = json.loads(json_bars_id.read().decode("utf-8"))
-    for bars in bars_id:
-        bars_dataset = list(bars.values())
-        for names in bars_dataset:
-            if names == "Бары":
-                return bars["Id"]
+    raw_datasets = urlopen(url_datasets)
+    all_datasets = json.loads(raw_datasets.read().decode("utf-8"))
+    for company_id in all_datasets:
+        for company_type in company_id.values():
+            if company_type == "Бары":
+                return company_id["Id"]
     return None
 
 
 if __name__ == '__main__':
-    url_bars_list = "http://api.data.mos.ru/"+\
-    "v{0}/datasets/{1}/rows?$orderby=SeatsCount".format(
+    url_bars_list = "{0}v{1}/datasets/{2}/{3}".format(
+        "http://api.data.mos.ru/",
         str(get_version()), 
-        str(get_id()))
+        str(get_id()),
+        "rows?$orderby=SeatsCount")
     bar_list = load_data(url_bars_list)
     biggest_bar = get_biggest_bar(bar_list)
     smallest_bar = get_smallest_bar(bar_list)
-    longitude = float(input("Input longitude"))
-    latitude = float(input("Input latitude"))
+    longitude = float(input("Input longitude  "))
+    latitude = float(input("Input latitude  "))
     closest_bar = get_closest_bar(bar_list, longitude, latitude)
     print()
     print()
     print("Biggest bar")
     print("-------------------------------")
-    print(biggest_bar)
+    print(biggest_bar["Cells"]["Name"])
     print("-------------------------------")
     print()
     print()
     print("Smallest bar")
     print("-------------------------------")
-    print(smallest_bar)
+    print(smallest_bar["Cells"]["Name"])
     print("-------------------------------")
     print()
     print()
     print("Closest bar")
     print("-------------------------------")
-    km = "%.3f" % closest_bar[1]
-    print(closest_bar[0]+"  ---  "+km+" км.")
+    print(closest_bar["Cells"]["Name"])
     print()
     print()
